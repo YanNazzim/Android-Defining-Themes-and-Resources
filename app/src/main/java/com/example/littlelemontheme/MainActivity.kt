4f57c135eeeb8e5e29f5af03d6d111bc43749d9f
Yan <ygonz42@gmail.com>
2024-04-05 11:05:31 -0400

    Lab Complete

---
+++ app/src/main/java/com/example/littlelemontheme/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.littlelemontheme
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.sp
+import com.example.littlelemontheme.ui.theme.LittleLemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                // A surface container using the 'background' color from the theme
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        Toast.makeText(this@MainActivity, "You clicked Order Now", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(
+                            text = "Order Now",
+                            color = Color.Black,
+                            fontSize = 48.sp,
+                            fontWeight = Bold
+                        )
+                    }
+                }
+
+                }
+
+            }
+        }
+    }
+
+
